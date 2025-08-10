f0b2bad45968e75ca536af5ebf45c612ebdd93cc
Luciano Abreu <lucianocaboculo@gmail.com>
2025-08-10 04:12:41 -0300

    feat: add footer

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,7 +1,12 @@
 const Footer = () => {
   return (
     <>
-      <footer className="p-5">FOOTER</footer>
+      <footer className="bg-accent w-full gap-1 p-5">
+        <p className="font-semibold">© 2025 Copyright BEWEAR</p>
+        <p className="text-muted-foreground block text-xs">
+          Todos os direitos reservados.
+        </p>
+      </footer>
     </>
   );
 };
